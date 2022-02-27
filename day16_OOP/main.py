6b6bc3f02df29450d4e7daee01e0d397af261e1d
hao134 <hao134@github.com>
2022-02-27 13:32:39 +0800

    test turtle

--- day16_OOP/main.py
+++ day16_OOP/main.py
@@ -1,27 +1,27 @@
-# from turtle import Turtle
-#
+from turtle import Turtle
+
 # import another_module
-#
+
 # print(another_module.another_variable)
+
+timmy = Turtle()
+print(timmy)
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 #
-# timmy = Turtle()
-# print(timmy)
+my_screen = Screen()
+# print(my_screen.canvheight)
+my_screen.exitonclick()
 
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("coral")
-# timmy.forward(100)
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name",["Pockchu", "Squirtle", "Charmander"])
+# table.add_column("Type",["Electric", "Water", "Fire"])
 #
-# my_screen = Screen()
-# # print(my_screen.canvheight)
-# my_screen.exitonclick()
-
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name",["Pockchu", "Squirtle", "Charmander"])
-table.add_column("Type",["Electric", "Water", "Fire"])
-
-table.align ="l"
-print(table)
+# table.align ="l"
+# print(table)
