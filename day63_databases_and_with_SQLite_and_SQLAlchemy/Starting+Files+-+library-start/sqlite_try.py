19f624d787d9044b3944a6eda3a0e2f11f36834f
hao134 <hao134@github.com>
2022-04-26 21:37:42 +0800

    flask and sqlite, create a database store books information

---
+++ day63_databases_and_with_SQLite_and_SQLAlchemy/Starting+Files+-+library-start/sqlite_try.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+db = sqlite3.connect("books-collection.db")
+
+cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, \
+#                 title varchar(250) NOT NULL UNIQUE, author \
+#                 varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J.K.R \
+               owing', '9.3')")
+db.commit()
