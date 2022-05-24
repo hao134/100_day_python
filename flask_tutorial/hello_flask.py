64d7902fda650e10f2cdbc0408ae562473042f45
hao134 <hao134@github.com>
2022-05-24 21:45:30 +0800

    send parameters using render template

--- flask_tutorial/hello_flask.py
+++ flask_tutorial/hello_flask.py
@@ -1,21 +1,9 @@
 from flask import Flask, url_for, redirect, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('abc.html')
-
-@app.route('/user/<username>')
-def username(username):
-    return 'i am ' + username
-
-@app.route('/a')
-def url_for_a():
-    return 'here is a'
-
-@app.route('/b')
-def b():
-    return redirect(url_for('url_for_a'))
+@app.route("/para/<user>")
+def index(user):
+    return render_template("abc.html", user_template = user)
 
 if __name__ == '__main__':
     app.debug = True
