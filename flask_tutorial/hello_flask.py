2c0b07b5d1ddbb1e37b8cfbecb035cded0952d05
hao134 <hao134@github.com>
2022-05-24 22:02:26 +0800

    POST AND GET

--- flask_tutorial/hello_flask.py
+++ flask_tutorial/hello_flask.py
@@ -1,9 +1,14 @@
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 app = Flask(__name__)
 
-@app.route("/para/<user>")
-def index(user):
-    return render_template("abc.html", user_template = user)
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        return "Hello " + request.values['username']
+
+    return "<form method='post' action='/login'><input type='text' name='username' />" \
+           "</br>" \
+           "<button type='submit'>Submit</button></form>"
 
 if __name__ == '__main__':
     app.debug = True
