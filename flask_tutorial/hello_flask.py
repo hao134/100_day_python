7cc530ad8e99be8da5f29d6e772f4315f4660d74
hao134 <hao134@github.com>
2022-05-25 20:19:03 +0800

    form action url_for

--- flask_tutorial/hello_flask.py
+++ flask_tutorial/hello_flask.py
@@ -1,14 +1,12 @@
 from flask import Flask, url_for, redirect, render_template, request
 app = Flask(__name__)
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/loginurl', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
         return "Hello " + request.values['username']
 
-    return "<form method='post' action='/login'><input type='text' name='username' />" \
-           "</br>" \
-           "<button type='submit'>Submit</button></form>"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.debug = True
