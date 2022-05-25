e0cbd07fdee40c80e8e2e9488e35da4733347729
hao134 <hao134@github.com>
2022-05-25 21:52:49 +0800

    flash message

--- flask_tutorial/hello_flask.py
+++ flask_tutorial/hello_flask.py
@@ -1,11 +1,19 @@
-from flask import Flask, url_for, redirect, render_template, request
+from flask import Flask, url_for, redirect, render_template, request, flash
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+def login_check(username, password):
+    if username == 'admin' and password == 'hello':
+        return True
+    else:
+        return False
 
 @app.route('/loginurl', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
-        return redirect(url_for('hello', username=request.form.get('username')))
-
+        if login_check(request.form['username'], request.form['password']):
+            flash('Login Success!')
+            return redirect(url_for('hello', username=request.form.get('username')))
     return render_template('login.html')
 
 @app.route('/hello/<username>')
@@ -13,5 +21,4 @@ def hello(username):
     return render_template('hello.html', username=username)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
