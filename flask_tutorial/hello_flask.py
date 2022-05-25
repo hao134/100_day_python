0882d1ce19d2287a6a605578f83b5bf2c8211535
hao134 <hao134@github.com>
2022-05-25 21:21:44 +0800

    redirect after some action

--- flask_tutorial/hello_flask.py
+++ flask_tutorial/hello_flask.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 @app.route('/loginurl', methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
-        return "Hello " + request.values['username']
+        return redirect(url_for('hello', username=request.form.get('username')))
 
     return render_template('login.html')
+
+@app.route('/hello/<username>')
+def hello(username):
+    return render_template('hello.html', username=username)
 
 if __name__ == '__main__':
     app.debug = True
