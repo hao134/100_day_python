e108862107c6f3ae0e5729510e683683e235082d
hao134 <hao134@github.com>
2022-07-12 21:53:30 +0800

    add stripe payment function

--- day96_eCommerce_website/main_day96.py
+++ day96_eCommerce_website/main_day96.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, jsonify
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterForm, LoginForm
@@ -6,6 +6,7 @@ from flask_wtf import FlaskForm
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import requests
+import stripe
 import os
 
 MOVIE_DB_API_KEY = "f23630e371240007466edc8cb63276a5"
@@ -14,10 +15,10 @@ MOVIE_DB_DISCOVER_URL = "https://api.themoviedb.org/3/discover/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://api.themoviedb.org/3/search/movie/https://image.tmdb.org/t/p/w500"
 DEFAULT_URL = "/discover/movie?sort_by=popularity.desc"
-STRIPE_KEYS = {
-    "secret_key": os.environ["STRIPE_SECRET_KEY"],
-    "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
-}
+# stripe keys
+stripe.api_key = os.environ["STRIPE_SECRET_KEY"]
+stripe_public_key = os.environ["STRIPE_PUBLISHABLE_KEY"]
+
 default_response = requests.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+MOVIE_DB_API_KEY)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -151,7 +152,92 @@ def show_single_movie(id):
     return render_template("single_movie.html", movie=get_movie[0],
                            authenticated = current_user.is_authenticated, message=message)
 
+@app.route("/add-to-cart/<int:id>")
+def add_to_cart(id):
+    if not current_user.is_authenticated:
+        return redirect(url_for("login"))
 
+    get_movie  = [movie for movie in all_movies if movie["id"] == id]
+    movie = get_movie[0]
+    new_cart_item = Cart(
+        product_id =movie["id"],
+        title=movie["title"],
+        image="https://image.tmdb.org/t/p/w500"+movie["poster_path"],
+        price=2,
+        is_purchased=False,
+        buyer=current_user.email
+    )
+    db.session.add(new_cart_item)
+    db.session.commit()
+    return redirect(url_for('show_single_movie', id = id))
+
+@app.route("/cart")
+def show_cart():
+    if not current_user.is_authenticated:
+        return redirect(url_for("login"))
+    global payable_amount
+    global cart_list
+    cart_list = ""
+    total_price = 0
+    all_items = Cart.query.filter_by(buyer = current_user.email, is_purchased = False).all()
+    for item in all_items:
+        total_price += item.price
+    discount = 1
+    if len(all_items) >= 5:
+        for _ in range(int(len(all_items)/5)):
+            discount *= 0.8
+    payable_amount = int(total_price * discount)
+    cart_list = ",".join([item.title for item in all_items])
+    return render_template("cart.html", cart=all_items, total_price = total_price, payable_amount=payable_amount,
+                           public_key = stripe_public_key, authenticated=current_user.is_authenticated)
+
+@app.route("/delete/<int:id>")
+def delete_item(id):
+    item = Cart.query.get(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for("show_cart"))
+
+@app.route("/create-checkout-session", methods=["POST"])
+@login_required
+def create_checkout_session():
+    domain_url = "http://127.0.0.1:5000/"
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data":{
+                "currency":"usd",
+                "product_data":{
+                    "name":cart_list,
+                },
+                "unit_amount":payable_amount*100,
+            },
+            "quantity":1,
+        }],
+        mode="payment",
+        success_url=domain_url+"success",
+        cancel_url=domain_url+"failed"
+    )
+    response = jsonify({"id":session.id})
+    return response
+
+@app.route("/success")
+@login_required
+def success():
+    global cart_list
+    non_purchased_items = Cart.query.filter_by(buyer=current_user.email, is_purchased=False).all()
+    for item in non_purchased_items:
+        id = item.id
+        movie = Cart.query.get(int(id))
+        movie.is_purchased = True
+        db.session.commit()
+    cart_list=""
+    return render_template("success.html", authenticated=current_user.is_authenticated)
+
+@app.route("/failed")
+@login_required
+def failed():
+    return render_template("cancel.html", authenticated=current_user.is_authenticated)
 
 if __name__ == '__main__':
     app.run(debug=True)
